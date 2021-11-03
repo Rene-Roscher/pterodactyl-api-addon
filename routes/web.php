2000b53106c30686763c0cfa0fde6e759a923036
Rene Roscher <roscher794@gmail.com>
2021-11-03 22:48:31 +0100

    fix: route prefix & middleware

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Xepare\PterodactylApiAddon\Http\Controllers\ApiKeyController;
 
-Route::group(['prefix' => '/users'], function () {
+Route::group(['prefix' => '/api/application/users', 'middleware' => ['api', 'throttle:api.application']], function () {
     /** Api-Keys */
     Route::get('{user}/api-keys', [ApiKeyController::class, 'index']);
     Route::post('{user}/api-keys', [ApiKeyController::class, 'store']);
