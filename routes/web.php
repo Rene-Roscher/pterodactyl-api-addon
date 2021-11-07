b9b7f658aed7d299c0979ee7a501c22b49067137
Rene Roscher <roscher794@gmail.com>
2021-11-07 23:24:56 +0100

    fix: routing

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,17 @@ use Illuminate\Support\Facades\Route;
 use Xepare\PterodactylApiAddon\Http\Controllers\ApiKeyController;
 use Xepare\PterodactylApiAddon\Http\Controllers\FreeAllocationController;
 
-Route::middleware(['api', 'throttle:api.application'])->group(function () {
+Route::prefix('/api/application')->middleware(['api', 'throttle:api.application'])->group(function () {
 
-    Route::group(['prefix' => '/api/application/users'], function () {
+    Route::group(['prefix' => '/users'], function () {
         /** Api-Keys */
         Route::get('{user}/api-keys', [ApiKeyController::class, 'index']);
         Route::post('{user}/api-keys', [ApiKeyController::class, 'store']);
         Route::delete('{user}/api-keys/{identifier}', [ApiKeyController::class, 'delete']);
     });
 
-    Route::group(['prefix' => '/{node}/allocations'], function () {
-        Route::get('/free', FreeAllocationController::class)->name('api.application.allocations');
+    Route::group(['prefix' => '/nodes/{node}/allocations'], function () {
+        Route::get('/free', FreeAllocationController::class)->name('api.application.allocations.free');
     });
 
 });
