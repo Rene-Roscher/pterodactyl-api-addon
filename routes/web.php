363f1b6cafb6179be96117b8882dec289ba8c876
Rene Roscher <roscher794@gmail.com>
2021-11-08 12:35:14 +0100

    add: recode for 'write' method of file manager

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Pterodactyl\Http\Controllers\Api\Client\Servers\FileController;
 use Xepare\PterodactylApiAddon\Http\Controllers\ApiKeyController;
 use Xepare\PterodactylApiAddon\Http\Controllers\FreeAllocationController;
+use Pterodactyl\Http\Middleware\Api\Client\Server\ResourceBelongsToServer;
+use Pterodactyl\Http\Middleware\Api\Client\Server\AuthenticateServerAccess;
 
 Route::prefix('/api/application')->middleware(['api', 'throttle:api.application'])->group(function () {
 
@@ -15,6 +18,18 @@ Route::prefix('/api/application')->middleware(['api', 'throttle:api.application'
 
     Route::group(['prefix' => '/nodes/{node}/allocations'], function () {
         Route::get('/free', FreeAllocationController::class)->name('api.application.allocations.free');
+    });
+
+});
+
+Route::middleware(['client-api', 'throttle:api.client'])->prefix('/api/client')->group(function () {
+
+    Route::group(['prefix' => '/servers/{server}', 'middleware' => [AuthenticateServerAccess::class, ResourceBelongsToServer::class]], function () {
+
+        Route::group(['prefix' => '/files'], function () {
+            Route::post('/update', FileController::class);
+        });
+
     });
 
 });
---
+++ src/Http/Controllers/FileController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Pterodactyl\Http\Controllers\Api\Client\Servers;
+
+use Carbon\CarbonImmutable;
+use Illuminate\Http\Response;
+use Pterodactyl\Models\Server;
+use Pterodactyl\Models\AuditLog;
+use Illuminate\Http\JsonResponse;
+use Pterodactyl\Services\Nodes\NodeJWTService;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Pterodactyl\Repositories\Wings\DaemonFileRepository;
+use Pterodactyl\Transformers\Daemon\FileObjectTransformer;
+use Pterodactyl\Http\Controllers\Api\Client\ClientApiController;
+use Pterodactyl\Http\Requests\Api\Client\Servers\Files\CopyFileRequest;
+use Pterodactyl\Http\Requests\Api\Client\Servers\Files\PullFileRequest;
+use Pterodactyl\Http\Requests\Api\Client\Servers\Files\ListFilesRequest;
+use Pterodactyl\Http\Requests\Api\Client\Servers\Files\ChmodFilesRequest;
+use Pterodactyl\Http\Requests\Api\Client\Servers\Files\DeleteFileRequest;
+use Pterodactyl\Http\Requests\Api\Client\Servers\Files\RenameFileRequest;
+use Pterodactyl\Http\Requests\Api\Client\Servers\Files\CreateFolderRequest;
+use Pterodactyl\Http\Requests\Api\Client\Servers\Files\CompressFilesRequest;
+use Pterodactyl\Http\Requests\Api\Client\Servers\Files\DecompressFilesRequest;
+use Pterodactyl\Http\Requests\Api\Client\Servers\Files\GetFileContentsRequest;
+use Xepare\PterodactylApiAddon\Http\Requests\WriteFileContentRequest;
+
+class FileController extends ClientApiController
+{
+    /**
+     * @var \Pterodactyl\Repositories\Wings\DaemonFileRepository
+     */
+    private $fileRepository;
+
+    /**
+     * FileController constructor.
+     */
+    public function __construct(
+        DaemonFileRepository $fileRepository
+    ) {
+        parent::__construct();
+
+        $this->fileRepository = $fileRepository;
+    }
+
+    /**
+     * Writes the contents of the specified file to the server.
+     *
+     * @throws \Pterodactyl\Exceptions\Http\Connection\DaemonConnectionException
+     */
+    public function __invoke(WriteFileContentRequest $request, Server $server): JsonResponse
+    {
+        $server->audit(AuditLog::SERVER__FILESYSTEM_WRITE, function (AuditLog $audit, Server $server) use ($request) {
+            $audit->subaction = 'write_content';
+            $audit->metadata = ['file' => $request->get('file')];
+
+            $this->fileRepository
+                ->setServer($server)
+                ->putContent($request->get('file'), $request->get('content'));
+        });
+
+        return new JsonResponse([], Response::HTTP_NO_CONTENT);
+    }
+
+}
