5e4333556501002c4ddd80e279b73f0ca75116fe
Rene-Roscher <43115138+Rene-Roscher@users.noreply.github.com>
2022-11-09 21:50:53 +0100

    Update ApiKeyController.php

--- src/Http/Controllers/ApiKeyController.php
+++ src/Http/Controllers/ApiKeyController.php
@@ -17,22 +17,22 @@ use Pterodactyl\Http\Requests\Api\Client\Account\StoreApiKeyRequest;
 
 class ApiKeyController extends ApplicationApiController
 {
-    /**
+    /
      * @var \Pterodactyl\Services\Api\KeyCreationService
      */
     private $keyCreationService;
 
-    /**
+    /
      * @var \Illuminate\Contracts\Encryption\Encrypter
      */
     private $encrypter;
 
-    /**
+    /
      * @var \Pterodactyl\Repositories\Eloquent\ApiKeyRepository
      */
     private $repository;
 
-    /**
+    /
      * ApiKeyController constructor.
      */
     public function __construct(
@@ -47,7 +47,7 @@ class ApiKeyController extends ApplicationApiController
         $this->repository = $repository;
     }
 
-    /**
+    /
      * Returns all of the API keys that exist for the given client.
      *
      * @return array
@@ -59,7 +59,7 @@ class ApiKeyController extends ApplicationApiController
             ->toArray();
     }
 
-    /**
+    /
      * Store a new API key for a user's account.
      *
      * @return array
@@ -67,20 +67,22 @@ class ApiKeyController extends ApplicationApiController
      * @throws \Pterodactyl\Exceptions\DisplayException
      * @throws \Pterodactyl\Exceptions\Model\DataValidationException
      */
-    public function store(User $user)
+    public function store(StoreApiKeyRequest $request, $user_id)
     {
-        dd($user);
-        $token = $user->createToken(
+
+  $tmp_user = User::find($user_id);
+
+        $token = $tmp_user->createToken(
             request('description'),
             request('allowed_ips')
         );
 
-        return $this->fractal->item($token->accessToken)
+        $result = $this->fractal->item($token->accessToken)
             ->transformWith($this->getTransformer(ApiKeyTransformer::class))
-            ->addMeta([
-                'secret_token' => $token->plainTextToken,
-            ])
             ->toArray();
+
+  $result["attributes"]["secret_token"] = $token->plainTextToken;
+  return $result;
     }
 
     /**
